2a96f2c6a8c15f9a328b9115233e132ec9b5b126
Ayo Adeyina <ayomideadeyina@MacBook-Pro-920.local>
2022-12-24 22:56:59 -0500

    new file update needed

---
+++ AutoMePls/Med.py
@@ -0,0 +1,14 @@
+import pyttsx3,PyPDF2
+
+pdfreader = PyPDF2.PdfFileReader(open('ACL_Pro.pdf', 'rb'))
+speaker = pyttsx3.init()
+
+for pg_num in range(pdfreader.numPages):
+    text = pdfreader.getPage(pg_num).extract_text()
+    clean_txt = text.strip().replace('\n', ' ')
+    print(clean_txt)
+
+speaker.save_to_file(clean_txt, 'medic.mp3')
+speaker.runAndWait()
+
+speaker.stop()
